93ba2adde75666d6167fbe873553b6c8af251592
Justin Smith <noisesmith@gmail.com>
2016-02-27 16:27:08 -0800

    1.0 release

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject bench "1.0.0"
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [criterium "0.4.4"]
+                 [org.clojure/core.async "0.2.374"]
+                 [com.cemerick/pomegranate "0.3.0"]
+                 [prismatic/schema "1.0.5"]])
