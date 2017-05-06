daeb2e61e356a9525310dd1ce1f800cced0cc483
Justin Smith <noisesmith@gmail.com>
2017-05-06 09:37:52 -0700

    updated deps

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject bench "1.0.0"
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha15"]
                  [criterium "0.4.4"]
-                 [org.clojure/core.async "0.2.374"]
-                 [com.cemerick/pomegranate "0.3.0"]
-                 [prismatic/schema "1.0.5"]])
+                 [org.clojure/core.async "0.3.442"]
+                 [com.cemerick/pomegranate "0.3.1"]
+                 [alembic "0.3.2"]
+                 [prismatic/schema "1.1.5"]])
