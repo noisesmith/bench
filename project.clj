f49d04daf6d2a4201e72988420d9ec45695d3c25
Justin Glenn Smith <noisesmith@gmail.com>
2017-10-30 09:51:54 -0700

    up to date deps

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject bench "1.0.0"
-  :dependencies [[org.clojure/clojure "1.9.0-alpha16"]
+  :dependencies [[org.clojure/clojure "1.9.0-beta3"]
                  [criterium "0.4.4"]
-                 [org.clojure/core.async "0.3.442"]
-                 [com.cemerick/pomegranate "0.3.1"]
+                 [org.clojure/core.async "0.3.443"]
+                 [com.cemerick/pomegranate "0.4.0"]
                  [alembic "0.3.2"]
-                 [prismatic/schema "1.1.5"]])
+                 [prismatic/schema "1.1.7"]])
