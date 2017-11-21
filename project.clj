90611ed3a4b10c9f7504b68f6dd5b86d23c447b2
Justin Glenn Smith <noisesmith@gmail.com>
2017-11-21 12:32:04 -0800

    misc deps

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject bench "1.0.0"
   :dependencies [[org.clojure/clojure "1.9.0-RC1"]
                  [criterium "0.4.4"]
-                 [org.clojure/core.async "0.3.443"]
-                 [com.cemerick/pomegranate "0.4.0"]
+                 [org.clojure/core.async "0.3.465"]
+                 [com.cemerick/pomegranate "1.0.0"]
                  [alembic "0.3.2"]
                  [prismatic/schema "1.1.7"]])
